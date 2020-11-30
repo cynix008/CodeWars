903899bf07818966921bda15820a6b8dc547e19e
DESKTOP-FHBMPG3\Hassan <hassan_humayun@hotmail.com>
2020-11-30 23:59:26 +0800

    Add project files.

---
+++ CodeWars/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CodeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string input = "Skippy";
+            //Console.Write($"Maskify {input}  { Maskify(input) }");
+            var numbers = new int[] {1, 7, 3, 10, 6, 8};
+            int target = 10;
+            var result  = TwoSums.GetTwoNumbers(numbers, target);
+            Console.WriteLine($"[{result[0]}, {result[1]}]");
+        }
+
+        public static string Maskify(string cc)
+        {
+            if (cc.Length <= 4 || string.IsNullOrEmpty(cc))
+            {
+                return cc;
+            }
+
+            var length = cc.Length;
+            var maskedString = string.Empty;
+            for (int i = 0; i < length; i++)
+            {
+                maskedString += (i < length - 4) ? "#" : cc[i].ToString();
+            }
+
+            return maskedString;
+        }
+
+    }
+}
---
+++ CodeWars/TwoSums.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeWars
+{
+    public class TwoSums
+    {
+        //1,3,4 = 7
+        public static int[] GetTwoNumbers(int[] numbers, int target)
+        {
+            Dictionary<int,int> numberToFind  =  new Dictionary<int, int>();
+            var resultEfficient = new int[2];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > target) continue;
+                var missingNumber =  target - numbers[i];
+
+                if (numberToFind.ContainsKey(missingNumber))
+                {
+                    resultEfficient[0] = numberToFind.GetValueOrDefault(missingNumber);
+                    resultEfficient[1] = i;
+                    return resultEfficient;
+                }
+
+                if (numberToFind.ContainsKey(numbers[i]))
+                {
+                    resultEfficient[0] = numberToFind.GetValueOrDefault(numbers[i]);
+                    resultEfficient[1] = i;
+                    return resultEfficient;
+                }
+                numberToFind.Add(missingNumber, i);
+            }
+
+            return resultEfficient;
+        }
+    }
+}
